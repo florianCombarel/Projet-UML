4ec0c630c4577ff3e7f513ee942ce677c00fd905
Etudiant L3 MIASHS INFO - 2017 - NICOLAS LEJEUNE <nicolas.lejeune@mathinfo.i-univ-tlse2.fr>
2017-12-11 16:32:18 +0100

    MODEL: PersonnePhysique

--- dev/application/src/model/personne/PersonnePhysique.java
+++ dev/application/src/model/personne/PersonnePhysique.java
@@ -1,5 +1,12 @@
 package model.personne;
 
 public class PersonnePhysique extends Personne {
-
+	
+	private String first_name;
+	
+	public PersonnePhysique(String name, String first_name,String adress, String telephone, String mail) {
+		super(name, adress, telephone, mail);
+		// TODO Auto-generated constructor stub
+		this.first_name = first_name;
+	}
 }
