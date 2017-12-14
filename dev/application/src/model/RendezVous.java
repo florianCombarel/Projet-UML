2d865167c5796b20eaca5fbbce3e9ad470a711be
Unknown <florian.combarel@orange.fr>
2017-12-14 18:02:13 +0100

    Auto stash before merge of "master" and "origin/master"
    début class rendez vous

--- dev/application/src/model/RendezVous.java
+++ dev/application/src/model/RendezVous.java
@@ -1,5 +1,13 @@
 package model;
 
+import java.util.Date;
+
+import model.bien_immobilier.BienImmobilier;
+import model.personne.Personne;
+
 public class RendezVous {
 
+	private Date dateVisite;
+	private Personne visiteur;
+	private BienImmobilier bienVisite;
 }
