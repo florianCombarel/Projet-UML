a62e63c5457fa836b1f2a49d1e8e15db7815af03
Unknown <florian.combarel@orange.fr>
2017-12-14 17:46:09 +0100

    Ajout du code de publicite et voeu

--- dev/application/src/model/Publicite.java
+++ dev/application/src/model/Publicite.java
@@ -1,5 +1,21 @@
 package model;
 
+import java.util.HashSet;
+
 public class Publicite {
 
+	private HashSet<String> documents;
+	
+	public Publicite(){
+		this.documents = new HashSet<>();
+		
+	}
+	
+	public void addDocument(String document){
+		this.documents.add(document);
+	}
+	
+	public void removeDocument(String document){
+		this.documents.remove(documents);
+	}
 }
--- dev/application/src/model/Voeu.java
+++ dev/application/src/model/Voeu.java
@@ -1,5 +1,69 @@
 package model;
 
 public class Voeu {
+	
+	private String typeBien;
+	private double prixSouhaite;
+	private String localisation;
+	private double surfaceAuSol;
+	private int nbPiece;
+	
+	public Voeu(String typeBien, double prixSouhaite, String localisation, double surfaceAuSol, int nbPiece){
+		switch(typeBien){
+		case "terrain":
+			this.prixSouhaite = Math.abs(prixSouhaite);
+			this.localisation = localisation;
+			this.surfaceAuSol = Math.abs(surfaceAuSol);
+			break;
+		case "maison":
+			this.prixSouhaite = Math.abs(prixSouhaite);
+			this.localisation = localisation;
+			this.surfaceAuSol = Math.abs(surfaceAuSol);
+			this.nbPiece = nbPiece;
+			break;
+		case "appartement":
+			this.nbPiece = nbPiece;
+			break;
+		}
+	}
 
+	public String getTypeBien() {
+		return typeBien;
+	}
+
+	public void setTypeBien(String typeBien) {
+		this.typeBien = typeBien;
+	}
+
+	public double getPrixSouhaite() {
+		return prixSouhaite;
+	}
+
+	public void setPrixSouhaite(double prixSouhaite) {
+		this.prixSouhaite = prixSouhaite;
+	}
+
+	public String getLocalisation() {
+		return localisation;
+	}
+
+	public void setLocalisation(String localisation) {
+		this.localisation = localisation;
+	}
+
+	public double getSurfaceAuSol() {
+		return surfaceAuSol;
+	}
+
+	public void setSurfaceAuSol(double surfaceAuSol) {
+		this.surfaceAuSol = surfaceAuSol;
+	}
+
+	public int getNbPiece() {
+		return nbPiece;
+	}
+
+	public void setNbPiece(int nbPiece) {
+		this.nbPiece = nbPiece;
+	}
 }
